b082344603616c629cc8fddd2cb99d1ca98cebb5
Federico <federicober@users.noreply.github.com>
2021-05-06 23:48:05 +0200

    Add basic tests for poetry, tox and pre-commit (#8)
    * Avoid pre-commit-hooks issues after creation
    
    * Test common commands
    
    * Change google pylintrc for custom pylintrc
    
    * small typos
    
    * removed darglint and commented all dev dependencies
    
    * add github actions
    
    Co-authored-by: foberndorfer <foberndorfer@eulidia.com>

---
+++ tests/test_dev_environment.py
@@ -0,0 +1,20 @@
+import subprocess
+import pathlib
+
+
+def test_pyproject_toml(default_generated_project):
+    subprocess.check_call(["poetry", "check"])
+
+
+def test_tox(tmp_generated_project: pathlib.Path):
+    subprocess.check_call(["tox"])
+
+
+def test_pre_commit(tmp_generated_project: pathlib.Path):
+    subprocess.check_call(["git", "init"])
+    subprocess.check_call(["pre-commit", "install"])
+    subprocess.check_call(["pre-commit", "run", "--all"])
+
+
+def test_build_wheel(tmp_generated_project: pathlib.Path):
+    subprocess.check_call(["poetry", "build"])

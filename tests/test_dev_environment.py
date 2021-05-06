f280eb42bf44da9b22991773c074a8bdc10acef8
foberndorfer <foberndorfer@eulidia.com>
2021-05-06 23:40:15 +0200

    removed darglint and commented all dev dependencies

--- tests/test_dev_environment.py
+++ tests/test_dev_environment.py
@@ -14,3 +14,7 @@ def test_pre_commit(tmp_generated_project: pathlib.Path):
     subprocess.check_call(["git", "init"])
     subprocess.check_call(["pre-commit", "install"])
     subprocess.check_call(["pre-commit", "run", "--all"])
+
+
+def test_build_wheel(tmp_generated_project: pathlib.Path):
+    subprocess.check_call(["poetry", "build"])

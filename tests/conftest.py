0fd962fc76131ca32ac13136851a518a298a5d7f
Federico <federicober@users.noreply.github.com>
2021-05-07 02:22:42 +0200

    Bugfix in postgen hook causing mkdocs to be ignored (#12)
    Co-authored-by: foberndorfer <foberndorfer@eulidia.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,25 @@
-import pytest
-import subprocess
 import contextlib
-import sys
 import os
 import pathlib
 import shutil
+import subprocess
+import sys
+
+import pytest
+from cookiecutter.main import cookiecutter
 
 _template_dir = pathlib.Path(__file__).parent.parent
+_base_cookiecutter_args = {
+    "project_name": "my-python-package",
+    "package_name": "my_python_package",
+    "friendly_name": "My Python Package",
+    "author": "Federico OBERNDORFER",
+    "email": "federico.ober@hotmail.com",
+    "github_user": "federicober",
+    "version": "0.1.0",
+    "dockerized": "false",
+    "docs_backend": "sphinx",
+}
 
 
 @contextlib.contextmanager
@@ -44,3 +57,19 @@ def tmp_generated_project(default_generated_project, tmp_path):
     shutil.copytree(default_generated_project, tmp_path, dirs_exist_ok=True)
     with change_dir(tmp_path):
         yield tmp_path
+
+
+@pytest.fixture()
+def custom_generated_project(tmp_path, request):
+    cookiecutter_args = _base_cookiecutter_args.copy()
+    if hasattr(request, "param"):
+        cookiecutter_args.update(request.param)
+    cookiecutter(
+        str(_template_dir),
+        output_dir=str(tmp_path),
+        no_input=True,
+        extra_context=cookiecutter_args,
+    )
+    project_dir = tmp_path / cookiecutter_args["project_name"]
+    with change_dir(project_dir):
+        yield project_dir
--- tests/test_documentation.py
+++ tests/test_documentation.py
@@ -1,8 +1,28 @@
 import os
+import subprocess
+
+import pytest
 
 
 def test_only_one_backend(default_generated_project):
-    file_in_docs = os.listdir('docs')
-    assert "__sphinx" not in file_in_docs
-    assert "__mkdocs" not in file_in_docs
-    assert "conf.py" not in file_in_docs or "mkdocs.yml" not in file_in_docs
+    files_in_docs = os.listdir("docs")
+    assert "__sphinx" not in files_in_docs
+    assert "__mkdocs" not in files_in_docs
+    assert "conf.py" not in files_in_docs or "mkdocs.yml" not in files_in_docs
+
+
+@pytest.mark.parametrize(
+    "custom_generated_project", [{"docs_backend": "mkdocs"}], indirect=True
+)
+def test_mkdocs_backend(custom_generated_project):
+    files_in_docs = os.listdir("docs")
+    assert "__sphinx" not in files_in_docs
+    assert "__mkdocs" not in files_in_docs
+    assert "conf.py" not in files_in_docs
+
+    assert "mkdocs.yml" in files_in_docs
+    assert "docs" in files_in_docs
+    md_files = os.listdir("docs/docs")
+    assert "index.md" in md_files
+
+    subprocess.check_call(["mkdocs", "build", "--strict", "-f", "docs/mkdocs.yml"])
